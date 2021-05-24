031b0bbbae2ccbeda3ce80c2ee57b72d08b6c355
athz95 <akhtarhassan1520@gmail.com>
2021-05-24 22:34:12 +0530

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,25 @@
+const productImage = document.querySelector(".product-image");
+const productButtons = document.querySelector(".products-buttons")
+
+
+function handleOpen() {
+    if (productButtons.style.display === "none") {
+        productButtons.style.display = "block";
+      } else {
+        productButtons.style.display = "none";
+      } 
+}
+
+function handleClose() {
+    if (productButtons.style.display === "none") {
+        productButtons.style.display = "block";
+      } else {
+        productButtons.style.display = "none";
+      } 
+}
+
+
+
+
+
+
